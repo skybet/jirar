67103409fef0de9575dc243f5d47e30cd88b4b6d
Rob Groves <Robin.Groves@skybettingandgaming.com>
2017-10-24 12:20:31 +0100

    pull columnColours from each Rmd into a sourced file: report/columnColours.R

---
+++ report/columnColours.R
@@ -0,0 +1,79 @@
+statusColours <- c("secondsInColumns.Open" = "white", "secondsInColumns.Backlog" = "white",
+                   "secondsInColumns.Selected.for.Development" = "white",
+                   "secondsInColumns.Analysis.In" = "red", "secondsInColumns.Analysis.Out" = "white",
+                   "secondsInColumns.Selected" = "red",
+                   "secondsInColumns.Selected.for.Release" = "red",
+                   "secondsInColumns.Release.Blocked" = "black",
+                                      "secondsInColumns.Next" = "red",
+
+                   "secondsInColumns.Test.Analysis.In" = "red", "secondsInColumns.Test.Analysis.Out" = "white",
+                   "secondsInColumns.Test.Analysis.Done" = "white",
+                   "secondsInColumns.Elaboration.In" = "red", "secondsInColumns.Elaboration.Out" = "white", "secondsInColumns.Elaboration..Out" = "white", "secondsInColumns.Elaboration.Done" = "white",
+                   "secondsInColumns.Elab.In" = "red", "secondsInColumns.Elab.Out" = "white",
+                   
+                   "secondsInColumns.3.Amigos.In" = "red", "secondsInColumns.3.Amigos.Out" = "white",
+                   "secondsInColumns.Implementation.In" = "orange", "secondsInColumns.Prioritised" = "orange",
+                   "secondsInColumns.Imp.In" = "orange",
+                   "secondsInColumns.Build.In" = "orange",
+
+ "secondsInColumns.Implementation.Out" = "white", 
+ "secondsInColumns.Imp.Out" = "white",
+ "secondsInColumns.Build.Done" = "white",
+
+ "secondsInColumns.Implementation.Done" = "white","secondsInColumns.Demo.d" = "white",
+                   "secondsInColumns.In.Progress" = "orange",
+                   
+                   "secondsInColumns.Review.In" = "#ffc966", "secondsInColumns.Review.Out" = "white",
+                   "secondsInColumns.Code.Review.In" = "#ffc966", "secondsInColumns.Code.Review.Out" = "white",
+                   "secondsInColumns.Tech.Review.In" = "#ffc966", "secondsInColumns.Tech.Review.Done" = "white",
+                   "secondsInColumns.Code.Review.Done" = "white",
+                   "secondsInColumns.Awaiting.Review" = "#ffc966", "secondsInColumns.Review.Done" = "white",                           "secondsInColumns.Raise.Change" = "#ffc966", "secondsInColumns.Change.Wait" = "white",
+
+                   "secondsInColumns.Code.Review...Demo.In" = "#ffc966", "secondsInColumns.Code.Review...Demo.Out" = "white",
+                   "secondsInColumns.In.Review" = "#ffc966",
+                   "secondsInColumns.Demo.In" = "#ffc966", "secondsInColumns.Demo.Out" = "white",
+                   "secondsInColumns.UAT.In" = "#ffc966", "secondsInColumns.UAT.Out" = "white",
+                   "secondsInColumns.UAT.Test.In" = "#ffc966", "secondsInColumns.UAT.Test.Done" = "white",
+                   "secondsInColumns.BR.In" = "#ffc966", "secondsInColumns.BR.Done" = "white",
+ 
+                   
+                   "secondsInColumns.Test.In" = "yellow", "secondsInColumns.Test.Out" = "white",
+                   "secondsInColumns.Testing.In" = "yellow", "secondsInColumns.Testing.Done" = "white",
+                   "secondsInColumns.Test.Done" = "white",
+                    "secondsInColumns.Functional.Test.In" = "yellow",
+                    "secondsInColumns.Functional.Test.Done" = "white",
+                   
+                   "secondsInColumns.Merged" = "green",
+                    "secondsInColumns.Merge.In" = "green",
+                    "secondsInColumns.Merge.Done" = "white",
+
+
+                   "secondsInColumns.Deploy.To.Test.In" = "#00bb00",
+                   "secondsInColumns.Functional.Test.Deployment.In" = "#00bb00",
+                   "secondsInColumns.Deploy" = "#00bb00",
+                   "secondsInColumns.Deploy.to.Test.Out" = "white",
+                   "secondsInColumns.Functional.Test.Deployment.Done" = "white",
+                   
+                   "secondsInColumns.Deployed.to.Test.Environment" = "#00bb00",
+                   "secondsInColumns.Deployed.to.Staging.Environment" = "#007700",
+                   "secondsInColumns.Deployed.to.Staging.Environmen" = "#007700",
+                   "secondsInColumns.Regression.Test.Deployment.In" = "#007700",
+                   "secondsInColumns.UAT.Test.Deployment.In" = "#007700",
+                   "secondsInColumns.Staging" = "#007700",
+
+                   "secondsInColumns.Regression.Test.Deployment.Done" = "white",
+                   "secondsInColumns.UAT.Test.Deployment.Done" = "white",
+                   "secondsInColumns.Regression.In" = "#ddffdd",
+                   "secondsInColumns.Regression.Done" = "white",
+
+
+                   "secondsInColumns.Ready.For.Release" = "#55bb55",
+                   "secondsInColumns.Release" = "#55bb55",
+                   "secondsInColumns.Change.Imp" = "#55bb55",
+                   "secondsInColumns.Release.Validation" = "#ddffdd",
+                   
+                   "secondsInColumns.Closed" = "#666666",
+                   "secondsInColumns.Resolved" = "#666666",
+                    "secondsInColumns.Done" = "#666666"
+
+)
